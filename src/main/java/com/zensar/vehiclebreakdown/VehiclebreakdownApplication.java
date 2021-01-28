1fcac4eca98d91b26fbe86dce1222596a9acd0e9
MuzikaMkhari <muzimkhari@yahoo.co.za>
2021-01-28 14:40:54 +0200

    uploaded the skeleton project

---
+++ src/main/java/com/zensar/vehiclebreakdown/VehiclebreakdownApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.vehiclebreakdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclebreakdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclebreakdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zensar/vehiclebreakdown/VehiclebreakdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.vehiclebreakdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclebreakdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
