b3528684bf5b586c48a77b2f4632daab64c9ebe9
mathapelo-motloung <mathapelo886@gmil.com>
2021-01-28 17:49:16 +0200

    update project structure

---
+++ src/main/java/com/zensar/vehiclebreakdown/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package com.zensar.vehiclebreakdown.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomePageController {
+	
+	@GetMapping("/home")
+	public String getIndex() {
+		return "index";
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Vehicle Breakdown Assistance</title>
+</head>
+<body>
+<h3>Welcome to Vehicle Breakdown Assistance</h3>
+</body>
+</html>
